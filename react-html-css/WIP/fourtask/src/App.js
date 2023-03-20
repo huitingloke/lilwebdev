b9c243b3e5eafa0d407257c065379d5bb74c1e8a
Beth <96629569+huitingloke@users.noreply.github.com>
2023-03-20 14:21:13 +0800

    Added content
    Added nice CSS themes, sorted out the list displays -- to add, make the survey work, add completed

--- react-html-css/WIP/fourtask/src/App.js
+++ react-html-css/WIP/fourtask/src/App.js
@@ -9,10 +9,25 @@ const LEVEL = {
 }
 
 let Storage = {
-  1: [],
-  2: [],
-  3: [],
-  4: []
+  1: [
+    ["whee", "sdfkjhaskdjfkajsdfkasdfjasldflasdjflkasdjflk", 1, false],
+    ["hi", "description", 1, false],
+    ["nerd", "description", 1, false]
+  ],
+  2: [
+    ["dfs", "description", 2, false],
+    ["sfasd", "description", 2, false],
+    ["sadf", "description", 2, false]
+  ],
+  3: [
+    ["sadffasd", "description", 3, false],
+    ["sdf", "description", 3, false],
+    ["asdfsd", "description", 3, false]
+  ],
+  4: [
+    ["whee", "description", 4, false],
+    ["hi", "description", 4, false],
+  ]
 }
 
 /**
@@ -31,19 +46,58 @@ let Storage = {
 
 function Task(props) {
   return (
-    <h1>sdafljkljkdfsa</h1>
+    <h1>somebody is having a mental breakdown and it is me</h1>
   );
 }
 
-function Container() {
+function Completed(props) {
   return (
-    <h1>well</h1>
+    <div>
+      <h1>Completed</h1>
+      <table>
+
+      </table>
+    </div>
+  );
+}
+
+function Container(props) {
+  return (
+    <div className='Containers'>
+      <h1>{LEVEL[props.level]}</h1>
+      <table className="tableManagement">
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Priority</th>
+          <th>Done?</th>
+        </tr>
+        {Storage[props.level].map((item) => (
+          <tr>
+            <td>{item[0]}</td>
+            <td>{item[1]}</td>
+            <td>{item[2]}</td>
+            <td><input type="checkbox" /></td>
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 }
 
 function ShowTask(props) {
   return (
-    <h1>HELP LAH</h1>
+    <div>
+      <div className="divSeparators">
+        <Container level="1" />
+        <Container level="2" />
+      </div>
+      <div className="divSeparators">
+        <Container level="3" />
+        <Container level="4" />
+      </div>
+    </div>
+
   );
 }
 
@@ -55,12 +109,15 @@ function App() { //do form logic inside here hehe
 
   const submit = (e) => {
     e.preventDefault();
-
+    level = level.toNumber();
+    Storage[level].push([name, description, level, false]);
+    level = "1";
   }
 
   return (
     <div>
-      <form>
+      <h1 id="title">The Essential Task Tracker</h1>
+      <form id="theForm">
         <label>Name</label>
         <input
           type="text"
@@ -77,11 +134,10 @@ function App() { //do form logic inside here hehe
           max="4"
           min="1"
         />
-
+        <input type="submit" />
       </form>
       <ShowTask />
-      <Container />
-      <Task />
+      <Completed />
     </div>
   );
 }
