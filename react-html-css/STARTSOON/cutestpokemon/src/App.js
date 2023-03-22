d3786ef0a852e422c7c4de9fd538a098f137f572
Beth <96629569+huitingloke@users.noreply.github.com>
2023-03-22 17:55:57 +0800

    Finished API logic for Pokemon, need to add hashing and CSS

--- react-html-css/STARTSOON/cutestpokemon/src/App.js
+++ react-html-css/STARTSOON/cutestpokemon/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+function Results(props) {
+
+  function Pokemon({ name, image, height, weight }) {
+
+    /*function generateCuteness() {
+      STILL NEED TO IMMPLEMENT THE CUTENEESS PLEASE
+    }*/
+
+    return (
+      <div>
+        <br />
+        <h1>{name}</h1>
+        <img src={image} height="150px" width="auto" alt="Pokemon" />
+        <h2>{height}ft, {weight} pounds</h2>
+        <h1>overall cuteness: 34534</h1>
+      </div>
+    );
+  }
+
+  let data = props.data;
+  if (!data) {
+    return (
+      <h1>choose a pokemon (please)</h1>
+    );
+  }
+
+  return (
+    <Pokemon 
+      name={data.name}
+      image={data.sprites.front_default}
+      height={data.height}
+      weight={data.weight}
+    />
+  );
+}
+
 function App() {
+
+  let [pokemon, setPokemon] = useState("");
+  let [data, setData] = useState();
+
+  const submit = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    ).then((response) => response.json()
+    ).then(setData);
+    if (data) {
+      setData(data);
+    }
+    setPokemon("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <h1>cute pokemon</h1>
+        <input 
+          type="text"
+          placeholder="choose a pokemon"
+          value={pokemon}
+          onChange={(event) =>
+            setPokemon(event.target.value)
+          }
+        />
+        <button>check!</button>
+      </form>
+      <Results data={data} />
     </div>
   );
 }
