10e44b13cef1b0fa2cd5bced3778cb2221f4c57a
Beth <96629569+huitingloke@users.noreply.github.com>
2023-03-23 11:40:33 +0800

    Updated the formula to calculate cuteness

--- react-html-css/STARTSOON/cutestpokemon/src/App.js
+++ react-html-css/STARTSOON/cutestpokemon/src/App.js
@@ -1,13 +1,49 @@
+//CHANGE BACKGROUND BASED ON COLOR??
+//MAKE THE CSS NICE
+
 import { useState } from "react";
 import './App.css';
 
+const COLORS = {
+  normal: "EEE6CE",
+  fire: "FEA8A8",
+  water: "CBE1FF",
+  grass: "AFDDB3",
+  electric: "FFFBA2",
+  ice: "D3FFF8",
+  fighting: "D5A3A3",
+  poison: "D7CAE9",
+  ground: "C1AC88",
+  flying: "D4C7E1",
+  psychic: "EBAACF",
+  bug: "CAE2A2",
+  rock: "C6BF93",
+  ghost: "CEB6D9",
+  dark: "B5A894",
+  dragon: "D1B7E8",
+  steel: "CACACA",
+  fairy: "FFE3E3"
+}
+
 function Results(props) {
 
-  function Pokemon({ name, image, height, weight }) {
+  function Pokemon({ name, image, height, weight, type }) {
 
-    /*function generateCuteness() {
-      STILL NEED TO IMMPLEMENT THE CUTENEESS PLEASE
-    }*/
+    //credits to geeksforgeeks for their code reference
+    function generateCuteness(string) {
+      let hash = 0;
+      if (string.length === 0) {
+        return 0;
+      }
+      for (let i = 0; i < string.length; i++) {
+        let character = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + character;
+        hash = hash & hash;
+      }
+      hash = Math.abs(hash);
+      hash = hash.toString();
+      return hash.slice(hash.length-2, hash.length-1);
+    }
 
     return (
       <div>
@@ -15,7 +51,8 @@ function Results(props) {
         <h1>{name}</h1>
         <img src={image} height="150px" width="auto" alt="Pokemon" />
         <h2>{height}ft, {weight} pounds</h2>
-        <h1>overall cuteness: 34534</h1>
+        <h2>main type: {type}</h2>
+        <h1>overall cuteness: {generateCuteness(name)}/10</h1>
       </div>
     );
   }
@@ -33,6 +70,7 @@ function Results(props) {
       image={data.sprites.front_default}
       height={data.height}
       weight={data.weight}
+      type={data.types[0].type.name}
     />
   );
 }
@@ -40,6 +78,7 @@ function Results(props) {
 function App() {
 
   let [pokemon, setPokemon] = useState("");
+  let [background, setBackground] = useState("FFFFFF");
   let [data, setData] = useState();
 
   const submit = (e) => {
@@ -55,7 +94,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: {background}}}>
       <form onSubmit={submit}>
         <h1>cute pokemon</h1>
         <input 
